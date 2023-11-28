177252ee5c8e4b621e69a894e690e9896099ac39
amanialmalki <amanialimalki@gmail.com>
2023-11-28 10:07:22 +0300

    Initial Commit

---
+++ ErthMC2/ErthMC2App.swift
@@ -0,0 +1,17 @@
+//
+//  ErthMC2App.swift
+//  ErthMC2
+//
+//  Created by Amani Almalki on 14/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ErthMC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
