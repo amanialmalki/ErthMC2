e321ef25944d63384f3f88850794c53870ba470d
amanialmalki <amanialimalki@gmail.com>
2023-11-29 10:39:12 +0300

    Updated

--- ErthMC2/MapView.swift
+++ ErthMC2/MapView.swift
@@ -11,69 +11,80 @@ import CloudKit
 struct MapView: View {
     var body: some View {
         ZStack {
-             Image("NewMap")
-                 .resizable()
-                 .scaledToFit()
-     //
-                Button(action: {
-                  print("Center clicked")
-                       }) {
-                           Rectangle()
-                               .fill(Color.red.opacity(0))
-                               .frame(width: 40,height: 80)
-                               .rotationEffect(Angle(degrees: -15))
-                               
-                         }.offset(x:0,y:12)
-          //
-                Button(action: {
-                  print("Right clicked")
-                       }) {
-                          
-                     HStack(spacing: 0)
-                     {
-                     Rectangle()
-                        .fill(Color.red.opacity(0))
-                        .frame(width: 40,height: 66)
-                     Rectangle()
-                          .fill(Color.red.opacity(0))
-                           .frame(width:40,height: 35)
-                     }
-                     }.offset(x: 80, y: 40)
+            Color("Background")
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("استكشف")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading, 200)
+                ZStack{
+                    Image("NewMap")
+                        .resizable()
+                        .scaledToFit()
             //
-            
-           
-                Button(action: {
-                  print("left clicked")
-                       }) {
-                           Rectangle()
+                       Button(action: {
+                         print("Center clicked")
+                              }) {
+                                  Rectangle()
+                                      .fill(Color.red.opacity(0))
+                                      .frame(width: 40,height: 80)
+                                      .rotationEffect(Angle(degrees: -15))
+                                      
+                                }.offset(x:0,y:12)
+                 //
+                       Button(action: {
+                         print("Right clicked")
+                              }) {
+                                 
+                            HStack(spacing: 0)
+                            {
+                            Rectangle()
                                .fill(Color.red.opacity(0))
-                               .frame(width: 40,height: 60)
-                               .rotationEffect(Angle(degrees: -15))
-                               
-                         }.offset(x: -60, y: 20)
+                               .frame(width: 40,height: 66)
+                            Rectangle()
+                                 .fill(Color.red.opacity(0))
+                                  .frame(width:40,height: 35)
+                            }
+                            }.offset(x: 80, y: 40)
+                   //
+                   
+                  
+                       Button(action: {
+                         print("left clicked")
+                              }) {
+                                  Rectangle()
+                                      .fill(Color.red.opacity(0))
+                                      .frame(width: 40,height: 60)
+                                      .rotationEffect(Angle(degrees: -15))
+                                      
+                                }.offset(x: -60, y: 20)
+                   
+                   //
+                       Button(action: {
+                         print("Botoom clicked")
+                              }) {
+                                  Rectangle()
+                                      .fill(Color.red.opacity(0))
+                                      .frame(width:55,height: 20)
+                                      
+                                }.offset(x: -6, y: 84)
+                   
+                 //
+                       Button(action: {
+                         print("Top Clicked")
+                              }) {
+                                  Rectangle()
+                                      .fill(Color.red.opacity(0))
+                                      .frame(width: 60,height: 50)
+                                      
+                                }.offset(x: -60, y: -66)
+                             
+                }
+                }
+            }
             
-            //
-                Button(action: {
-                  print("Botoom clicked")
-                       }) {
-                           Rectangle()
-                               .fill(Color.red.opacity(0))
-                               .frame(width:55,height: 20)
-                               
-                         }.offset(x: -6, y: 84)
-            
-          //
-                Button(action: {
-                  print("Top Clicked")
-                       }) {
-                           Rectangle()
-                               .fill(Color.red.opacity(0))
-                               .frame(width: 60,height: 50)
-                               
-                         }.offset(x: -60, y: -66)
-                      
-         }
-        .padding()
+
     }//end body    
 }
 
