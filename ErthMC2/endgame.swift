5b63293d7a9297480fb2d1998ec906dc3dc7e721
mnerahawn <mnerahawncs@gmail.com>
2023-11-30 10:23:43 +0300

    new

---
+++ ErthMC2/endgame.swift
@@ -0,0 +1,18 @@
+//
+//  endgame.swift
+//  ErthMC2
+//
+//  Created by mnerah awn on 16/05/1445 AH.
+//
+
+import SwiftUI
+
+struct endgame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    endgame()
+}
---
+++ ErthMC2/startpage.swift
@@ -0,0 +1,18 @@
+//
+//  startpage.swift
+//  ErthMC2
+//
+//  Created by mnerah awn on 16/05/1445 AH.
+//
+
+import SwiftUI
+
+struct startpage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    startpage()
+}
