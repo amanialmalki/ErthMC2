507ffca98dd5dd09ab28776f021bf41a00c5d466
mnerahawn <mnerahawncs@gmail.com>
2023-11-30 10:28:38 +0300

    new

--- ErthMC2/endgame.swift
+++ ErthMC2/endgame.swift
@@ -4,15 +4,3 @@
 //
 //  Created by mnerah awn on 16/05/1445 AH.
 //
-
-import SwiftUI
-
-struct endgame: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    endgame()
-}
--- ErthMC2/startpage.swift
+++ ErthMC2/startpage.swift
@@ -4,15 +4,36 @@
 //
 //  Created by mnerah awn on 16/05/1445 AH.
 //
-
 import SwiftUI
 
-struct startpage: View {
+struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Color("Background")
+                .ignoresSafeArea()
+            VStack {
+                Image("صقر")
+                    .resizable()
+                    . frame(width: 200,height: 200)
+                    .foregroundColor(.green)
+                  
+                Button{
+                           } label: {
+                               Text("ابدا اللعب ")
+                                   .foregroundColor(.white)
+                                   .frame(width: 215, height: 69)
+                                   .background(Color.button)
+                                   .foregroundColor(.gray)
+                                   .cornerRadius(13)
+                               /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                           }
+                }
+            .padding(.bottom, 21.0)
+        }
     }
+    
+}
+#Preview {
+    ContentView()
 }
 
-#Preview {
-    startpage()
-}
