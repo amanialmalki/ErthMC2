d648b188c9f65a3fb615f39f5f97648c4f61a40c
amanialmalki <amanialimalki@gmail.com>
2023-12-03 09:57:42 +0300

    Splash

--- ErthMC2/ErthMC2App.swift
+++ ErthMC2/ErthMC2App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ErthMC2App: App {
     var body: some Scene {
         WindowGroup {
-            MapView()
+            SplashScreen()
         }
     }
 }
--- ErthMC2/SplashScreen.swift
+++ ErthMC2/SplashScreen.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State var isActive : Bool = false
+        @State private var size = 0.8
+        @State private var opacity = 0.5
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if isActive {
+                  startpage()
+              }else {
+       
+                  ZStack{
+                      Color("Background")
+                          .ignoresSafeArea(.all)
+                      
+                      VStack{
+                          //LOGO START HERE
+                          Image("صقر")
+                              .resizable()
+                              .scaledToFit()
+                              .offset(x:0,y:-30)
+                              .frame(width: 350,height: 480)
+                      }
+                      // animation starts here
+                                     .scaleEffect(size)
+                                     .opacity(opacity)
+                                     .onAppear{
+                                         withAnimation(.easeIn(duration: 1.1)){
+                                             self.size = 1.0
+                                             self.opacity = 1.00
+                                         }
+                                         
+                                     } // 1st on appear ends here
+                                     
+                                 } // zstack ends here
+                  .onAppear {
+                                 DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                                     withAnimation {
+                                         self.isActive = true
+                                         
+                                     }
+                                     
+                                 }
+                                 
+                             } // 2ed on appear ends here
+                             
+                         } // if ends here
     }
 }
 
