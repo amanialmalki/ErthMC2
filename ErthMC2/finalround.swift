2127e7c60c05510e8c6ee428a01303e2b251d992
mnerahawn <mnerahawncs@gmail.com>
2023-12-03 09:32:32 +0300

    new

---
+++ ErthMC2/finalround.swift
@@ -0,0 +1,18 @@
+//
+//  finalround.swift
+//  ErthMC2
+//
+//  Created by mnerah awn on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+struct finalround: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    finalround()
+}
