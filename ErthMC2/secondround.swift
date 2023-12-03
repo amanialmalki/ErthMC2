0e308dd92779aa3682ece3499a60b6f810896cca
mnerahawn <mnerahawncs@gmail.com>
2023-12-03 09:20:07 +0300

    new

---
+++ ErthMC2/secondround.swift
@@ -0,0 +1,18 @@
+//
+//  secondround.swift
+//  ErthMC2
+//
+//  Created by mnerah awn on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+struct secondround: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    secondround()
+}
