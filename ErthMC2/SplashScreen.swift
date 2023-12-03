440e782b4a3eba3eab5fd95c2b820e94c8fa80ec
amanialmalki <amanialimalki@gmail.com>
2023-12-03 09:51:06 +0300

    SplashScreen

---
+++ ErthMC2/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  ErthMC2
+//
+//  Created by Amani Almalki on 19/05/1445 AH.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
